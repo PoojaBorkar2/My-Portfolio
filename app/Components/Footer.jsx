2244e6ec393f6c070f9b32506ea3d9d6678005db
PoojaBorkar2 <poojab2805@gmail.com>
2025-06-16 12:34:47 +0530

    the footer file is added

---
+++ app/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer border border-t-[#33353f] border-1-transparent text-white'>
+        <div className='container p-5 flex justify-between'>
+            <span className='px-22'>PORTFOLIO</span>
+            <p className='text-slate-500 '>All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
